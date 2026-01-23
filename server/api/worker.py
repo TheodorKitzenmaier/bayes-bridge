728cd26fb47a5d55ed6127284be8db4fd0c7eb23
COM8AT <tkitzenm@asu.edu>
2026-01-22 17:17:36 -0700

    Spaces, Dummy Endpoint

--- server/api/worker.py
+++ server/api/worker.py
@@ -2,6 +2,11 @@ import flask
 import flask_restx
 
 worker_namespace = flask_restx.Namespace(
-	"worker",
-	"Endpoints to start and monitor model workers."
+    "worker",
+    "Endpoints to start and monitor model workers."
 )
+
+@worker_namespace.route("/start")
+class StartWorker(flask_restx.Resource):
+    def get(self):
+        return {"test": True}
