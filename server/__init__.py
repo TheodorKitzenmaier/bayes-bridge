97e76160d8c654e1f60f4e34782dca252c72f38e
COM8AT <tkitzenm@asu.edu>
2026-02-19 22:57:43 -0700

    Create temp paths

--- server/__init__.py
+++ server/__init__.py
@@ -1,10 +1,13 @@
 import flask
+import pathlib
 
 from .api import api
+from .utils.types import WORKER_FILE_PATH
 
 # Create app
 app = flask.Flask(__name__)
 app.register_blueprint(api, url_prefix=f"/{api.name}")
 
 def run():
+    pathlib.Path(WORKER_FILE_PATH).mkdir(mode=777,parents=True,exist_ok=True)
     app.run()
