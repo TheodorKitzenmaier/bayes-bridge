2063d5204b361a78fc0540dc71efe44d079f4596
COM8AT <tkitzenm@asu.edu>
2026-02-20 00:41:36 -0700

    Fix perms

--- server/__init__.py
+++ server/__init__.py
@@ -9,6 +9,5 @@ app = flask.Flask(__name__)
 app.register_blueprint(api, url_prefix=f"/{api.name}")
 
 def run():
-    pathlib.Path(WORKER_FILE_PATH).mkdir(mode=777,parents=True,exist_ok=True)
-    pathlib.Path(WORKER_FILE_PATH).chmod(777)
+    pathlib.Path(WORKER_FILE_PATH).mkdir(parents=True,exist_ok=True)
     app.run()
