b31c44d45c55239b806c74b781f1c370389536b2
COM8AT <tkitzenm@asu.edu>
2026-03-25 13:01:41 -0700

    No Zoids

--- server_c/worker.cc
+++ server_c/worker.cc
@@ -1,6 +1,23 @@
 #include <cstdio>
+#include <signal.h>
 
 #include "worker.h"
+
+WorkerMap::WorkerMap():
+    worker_mutex(),
+    workers() {
+}
+
+WorkerMap::~WorkerMap() {
+  // Don't start a zombie apocalypse.
+  for(auto worker: workers) {
+    if (worker.second->pid) {
+      kill(worker.second->pid, SIGKILL);
+      waitpid(worker.second->pid, nullptr, 0);
+    }
+    delete worker.second;
+  }
+}
 
 Worker* WorkerMap::GetWorker(uint64_t t_id) {
   const std::lock_guard<std::mutex> lock(this->worker_mutex);
